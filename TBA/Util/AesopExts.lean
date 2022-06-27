95dc6d88e1a4b31ffe198bf8cda8c8f0c331f840
Sebastian Ullrich <sebasti@nullri.ch>
2022-06-27 10:13:15 +0200

    update Aesop & slides

--- TBA/Util/AesopExts.lean
+++ TBA/Util/AesopExts.lean
@@ -6,37 +6,6 @@ open Lean
 open Lean.Meta
 open Lean.Elab.Tactic
 
-@[aesop norm tactic]
-def substVars : TacticM Unit :=
-  liftMetaTactic fun mvarId => do
-    for localDecl in (← getLCtx) do
-      if let some (_, mvarId) ← observing? (substCore mvarId localDecl.fvarId <|> substCore mvarId localDecl.fvarId (symm := true)) then
-        return [mvarId]
-    throwError "nope"
-
-@[aesop safe 100 tactic]
-def split : TacticM Unit :=
-  liftMetaTactic fun mvarId => do
-    if let some mvarIds ← splitTarget? mvarId then
-      return mvarIds
-    throwError "nope"
-
-@[aesop safe 1000 tactic]
-def splitAt : TacticM Unit :=
-  liftMetaTactic fun mvarId => do
-    for localDecl in (← getLCtx) do
-      if let some mvarIds ← splitLocalDecl? mvarId localDecl.fvarId then
-        return mvarIds
-    throwError "nope"
-
-def simpAll : TacticM Unit := do
-  let m ← getMainGoal
-  evalTactic (← `(tactic| simp_all))
-  if (← getGoals).isEmpty then
-    return
-  if (← getMainGoal) == m || (← inferType (mkMVar (← getMainGoal))) == (← inferType (mkMVar m)) then
-    throwError "nope"
-
 def elimAny : Aesop.RuleTac := fun input => do
   for localDecl in (← getLCtx) do
     if localDecl.isAuxDecl then
